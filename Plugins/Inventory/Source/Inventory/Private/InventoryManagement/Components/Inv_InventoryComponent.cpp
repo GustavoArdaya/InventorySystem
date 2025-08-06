1b7e4f7088f4449e7e29ccdd5ff43349b429cb34
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-08-06 13:29:15 +0200

    Add Inventory Component

---
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+#include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
+
+UInv_InventoryComponent::UInv_InventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UInv_InventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ConstructInventory();
+	
+}
+
+void UInv_InventoryComponent::ConstructInventory()
+{
+	OwningController = Cast<APlayerController>(GetOwner());
+	checkf(OwningController.IsValid(), TEXT("OwningController is null in UInv_InventoryComponent::ConstructInventory. Make sure the component is attached to a valid actor with a player controller."));
+	if (!OwningController->IsLocalController()) return;
+
+	InventoryMenu = CreateWidget<UInv_InventoryBase>(OwningController.Get(), InventoryMenuClass);
+	InventoryMenu->AddToViewport();
+}
+
